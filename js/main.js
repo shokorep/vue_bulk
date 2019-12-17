b50478d227f4fdda0cd89ebbce322a4bd77ba370
Shoko Miyagawa <s.mygw@icloud.com>
2019-12-17 20:16:19 +0900

    todo app

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var app = new Vue({
+    el:'#app',
+    data: {
+        message: 'hello',
+        url: 'https://jp.vuejs.org/',
+        toggle: false,
+        languages: ['java', 'Ruby', 'Python'],
+
+        todoItem:'',
+        todos: []
+    },
+    methods: {
+        showSite() {
+            this.toggle ? this.toggle = false : this.toggle = true
+        },
+        onClick() {
+            this.message = 'clicked'
+        },
+        addList(event) {
+            // alert(event)
+            if (this.todoItem) {
+                var todo = {
+                    item: this.todoItem,
+                    isDone: false
+                }
+                this.todos.push(todo)
+                this.todoItem = ''
+            }
+        },
+        deliteItem() {
+
+        }
+    }
+})
