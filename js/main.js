da1009096241e927dafb5550c0ac7278040a7716
Shoko Miyagawa <s.mygw@icloud.com>
2019-12-18 20:08:31 +0900

    get api

--- js/main.js
+++ js/main.js
@@ -7,7 +7,37 @@ var app = new Vue({
         languages: ['java', 'Ruby', 'Python'],
 
         todoItem:'',
-        todos: []
+        todos: [],
+
+        bpi: null,
+        hasError: false,
+        isLoading: true,
+
+        reverseMessage: 'Hello, Vue.js!',
+        colorRedMessage: 'Hello,<span style="color:red;"> vue.js</span>',
+
+        number: 1000000,
+        ok: false
+    },
+    mounted() {
+        axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(res =>{
+            console.log({res})
+            this.bpi = res.data.bpi
+            console.log(this.bpi)
+        }).catch(err => {
+            // console.log(err)
+            this.hasError = true
+        }).finally(
+            this.isLoading = false
+        ) 
+    },
+    filters: {
+        currencyDecimal(value){
+            return value.toFixed(2)
+        },
+        numberFormat(value) {
+            return value.toLocaleString();
+        }
     },
     methods: {
         showSite() {
@@ -15,6 +45,9 @@ var app = new Vue({
         },
         onClick() {
             this.message = 'clicked'
+        },
+        onReverse() {
+            this.reverseMessage = this.reverseMessage.split('').reverse().join('')
         },
         addList(event) {
             // alert(event)
@@ -26,6 +59,9 @@ var app = new Vue({
                 this.todos.push(todo)
                 this.todoItem = ''
             }
+        },
+        changeOk() {
+            this.ok ? this.ok = false : this.ok = true
         },
         deliteItem(index) {
             // alert(index)
